797949120eb7db06b6c75a9e3383a018352532f5
Miracle Mayowa Adebunmi <36198643+miracle5284@users.noreply.github.com>
2025-04-07 19:22:26 +0100

    Restructure the django arch as a service-style for microservice prep

--- chrona-backend/config/settings/prod.py
+++ chrona-backend/config/settings/prod.py
@@ -5,8 +5,8 @@ from .base import *
 DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
-CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS
+# CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+# CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS
 
 # Production database config example (PostgreSQL)
 DATABASES = {
@@ -48,4 +48,48 @@ STATICFILES_DIRS = []
 timer_static = BASE_DIR / 'chrona' / 'static'
 if timer_static.exists():
     STATICFILES_DIRS.append(timer_static)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s] [%(levelname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+        'json': {
+            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s',
+        },
+    },
+
+    'handlers': {
+        'redis': {
+            'level': LOG_LEVEL,
+            'class': 'config.logging_handlers.RedisStreamHandler',
+            'formatter': 'json',
+        },
+
+    },
+
+    'root': {
+        'handlers': ['console', 'redis'],
+        'level': LOG_LEVEL,
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': LOG_LEVEL,
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
 
