a25b2ad4ea1523344d08f5b779c58523f6d6818b
Miracle Mayowa Adebunmi <36198643+miracle5284@users.noreply.github.com>
2025-05-24 17:02:10 +0100

    Fix user logout: access token should be blacklisted on logging out.

--- chrona-backend/config/settings/base.py
+++ chrona-backend/config/settings/base.py
@@ -158,6 +158,7 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
+    'TOKEN_BLACKLIST_ENABLED': True
 }
 INSTALLED_APPS += ['rest_framework_simplejwt.token_blacklist']
 
